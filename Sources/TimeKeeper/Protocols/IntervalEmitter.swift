99009333ab314c9c9049e14399c895f2476dbd59
reuschj <reuschj@gmail.com>
2019-11-03 01:13:35 -0600

    Overhaul of TimeEmitter

---
+++ Sources/TimeKeeper/Protocols/IntervalEmitter.swift
@@ -0,0 +1,71 @@
+//
+//  IntervalEmitter.swift
+//  AnalogClock
+//
+//  Created by Justin Reusch on 11/3/19.
+//  Copyright © 2019 Justin Reusch. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+/**
+ A protocol to define common characteristics of an emitter that emits according to an interval
+ */
+@available(OSX 10.15, *)
+@available(iOS 13.0, *)
+public protocol IntervalEmitter: ObservableObject, Timed, Updatable, Hashable {
+    
+    /// The time interval on which to emit the current time and hand rotations
+    var interval: TimeInterval { get set }
+    
+    /// A timer that will drive the updates
+    var timer: Timer! { get set }
+}
+
+/**
+ Default method implementations for `IntervalEmitter`
+ */
+@available(OSX 10.15, *)
+@available(iOS 13.0, *)
+extension IntervalEmitter {
+    /**
+     Starts the timer with the specified interval and sets it's update action
+     - Parameters:
+        - interval: The interval which the emitter will update the current time and date
+     */
+    public func startTimer(withTimeInterval interval: TimeInterval) {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true, block: { [weak self] timer in
+            self?.update()
+        })
+    }
+    
+    /**
+     Starts the timer using the interval property specified in this class
+     - Parameters:
+        - interval: The interval which the emitter will update the current time and date
+     */
+    public func startTimer() {
+        startTimer(withTimeInterval: interval)
+    }
+    
+    /**
+     Changes the time interval (by restarting the timer)
+     - Parameters:
+        - interval: The interval which the emitter will update the current time and date
+     */
+    public func updateTimerInterval(to interval: TimeInterval) {
+        startTimer(withTimeInterval: interval)
+    }
+    
+    /// Stops the timer
+    public func stopTimer() {
+        timer?.invalidate()
+    }
+    
+    /// Hash method
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(interval)
+    }
+}
--- Sources/TimeKeeper/TimeEmitter.swift
+++ Sources/TimeKeeper/TimeEmitter.swift
@@ -10,11 +10,11 @@ import SwiftUI
 import Combine
 
 /**
- Emits the current time and clock hand position at the specified time interval
+ Emits the current time at the specified time interval
  */
 @available(OSX 10.15, *)
 @available(iOS 13.0, *)
-open class TimeEmitter: ObservableObject, Timed, Updatable, Hashable {
+public class TimeEmitter: IntervalEmitter {
     
     /// The current time to emit with accessors to all time components
     @Published public var time: TimeKeeper
@@ -25,65 +25,24 @@ open class TimeEmitter: ObservableObject, Timed, Updatable, Hashable {
     }
     
     /// A timer that will drive the updates
-    private var timer: Timer!
+    public var timer: Timer!
     
     /**
      Initializer specifying the interval directly
      - Parameters:
         - interval: The interval which the emitter will update the current time and date
-        - startTimer: Allows opting out of starting the timer during initialization
      */
-    public init(updatedEvery interval: TimeInterval = defaultTickInterval, startTimer: Bool = true) {
+    public init(updatedEvery interval: TimeInterval = defaultTickInterval) {
         self.timer = nil
         self.interval = interval
         self.time = TimeKeeper()
-        if startTimer { self.startTimer() }
-    }
-    
-    /**
-     Starts the timer with the specified interval and sets it's update action
-     - Parameters:
-     - interval: The interval which the emitter will update the current time and date
-     */
-    public func startTimer(withTimeInterval interval: TimeInterval) {
-        timer?.invalidate()
-        timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true, block: { [weak self] timer in
-            self?.update()
-        })
-    }
-    
-    /**
-     Starts the timer using the interval property specified in this class
-     - Parameters:
-     - interval: The interval which the emitter will update the current time and date
-     */
-    public func startTimer() {
-        startTimer(withTimeInterval: interval)
-    }
-    
-    /**
-     Changes the time interval (by restarting the timer)
-     - Parameters:
-     - interval: The interval which the emitter will update the current time and date
-     */
-    public func updateTimerInterval(to interval: TimeInterval) {
-        startTimer(withTimeInterval: interval)
-    }
-    
-    /// Stops the timer
-    public func stopTimer() {
-        timer?.invalidate()
+        self.startTimer()
     }
     
     /// Updates the time and emits
     public func update() {
         time.update()
         objectWillChange.send()
-    }
-    
-    /// Hash method
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(interval)
     }
     
     /// Equatable method
